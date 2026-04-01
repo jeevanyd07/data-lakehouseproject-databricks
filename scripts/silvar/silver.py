bf0d94be1c6c5fb765e9375447c9b1a51065a528
JEEVAN Y D <jeevanyd07@gmail.com>
2026-04-01 22:49:21 +0530

    Update silver.py

--- scripts/silvar/silver.py
+++ scripts/silvar/silver.py
@@ -67,10 +67,80 @@ WHERE _c2 != 'Date_Inv'
 
 df.write.mode("overwrite").saveAsTable("silver.dealer_invoices")
 
+----------------------------------------------------------------------------------------------------------------
+## Reading Bronze Table 2 bronze.dealer_workshop_performance 
+-----------------------------------------------------------------------------------------------------------------
+df = spark.table("workspace.bronze.dealer_workshop_performance")
 
 ----------------------------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------------------------
+## Silver Transformations & Loading 2: silver.dealer_workshop_performance  
 
+### 1. Convert the date format to SQL format for the columns **Date_Inv** and **Job Date**.
+### 2. If **Insurance Party** is NULL, show it as **"NA"**.
+### 3. Remove the unwanted columns.
+### 4.Add an additional column named Network_Type.  
+-----------------------------------------------------------------------------------------------------------------
+df = spark.sql("""
+    SELECT
+_c0 AS Fiscal_Year,
+_c1 AS Day,
+_c2 AS Month,
+_c3 AS Job_Card_Number,
+_c4 AS Invoice_Number,
+ date_format(
+        COALESCE(
+            try_to_date(_c5, 'M/d/yy'),
+            try_to_date(_c5, 'd/M/yy'),
+            try_to_date(_c5, 'dd-MM-yyyy'),
+            try_to_date(_c5, 'dd/MM/yyyy')
+        ),
+        'yyyy-MM-dd'
+    ) AS Job_In_Date,
+date_format(
+        COALESCE(
+            try_to_date(_c6, 'M/d/yy'),
+            try_to_date(_c6, 'd/M/yy'),
+            try_to_date(_c6, 'dd-MM-yyyy'),
+            try_to_date(_c6, 'dd/MM/yyyy')
+        ),
+        'yyyy-MM-dd'
+    ) AS Job_Out_Date,
+CASE 
+      WHEN _c8 IN ('KA010001','TN310001','TS010011','TN310002','AP040001','AP020002') THEN 'FICOCO'
+      ELSE 'FIFO'
+    END AS Network_Type,
+_c8 AS Network_Code,
+_c9 AS Network_Name,
+_c10 AS Dealer_State,
+_c12 AS Service_Type,
+_c13 AS ODO_Reading,
+_c14 AS Network_Location,
+_c15 AS Location_Code,
+_c16 AS Customer_Name,
+_c17 AS Phone_Number,
+_c18 AS Job_Card_Status,
+_c19 AS Job_Source,
+TRIM(_c22) AS Registration_Number,
+_c23 AS Model_Category,
+_c24 AS Brand_Name,
+_c25 AS Model_Name,
+_c26 AS Technician_Name,
+_c28 AS Service_Advisor_Name,
+_c31 AS Labour_Revenue,
+_c32 AS Parts_Revenue,
+_c34 AS Oil_Revenue,
+_c36 AS Accessories_Revenue,
+_c40 AS Discount_Amount,
+_c41 AS Total_Job_Card_Revenue,
+CASE 
+      WHEN _c42 IS NULL THEN 'NA'
+      ELSE _c42
+    END AS Insurance_Party
+FROM workspace.bronze.dealer_workshop_performance
+WHERE _c5 != 'Job_Card_Open_Date'
+""")
 
+df.write.mode("overwrite").saveAsTable("silver.dealer_workshop_performance")
 
+------------------------------------------------------------------------------------------------------
 
