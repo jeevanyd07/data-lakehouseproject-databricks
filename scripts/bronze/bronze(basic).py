08c2992dc327001388b8d6cbd3e0bbf1bdc0b458
JEEVAN Y D <jeevanyd07@gmail.com>
2026-04-12 19:52:55 +0530

    Update bronze(basic).py

--- scripts/bronze/bronze(basic).py
+++ scripts/bronze/bronze(basic).py
@@ -11,14 +11,26 @@ print([m for m in methods if 'xl' in m.lower() or 'excel' in m.lower()])
 
 ## Reading ERP Files from Excle
 ### Reading 1.Dealer_Invoice_Report.excle
-df = (spark.read
-      .option("header","true")
-      .option("inferSchema","true")
-      .excel("/Volumes/workspace/bronze/source_system/ERP_FILES/Dealer_Invoice_Report.xlsx"))
-df.display()
+# Read File 1
+df1 = (spark.read
+       .option("header", "true")
+       .option("inferSchema", "true")
+       .excel("/Volumes/workspace/bronze/source_system/ERP_FILES/Dealer_Invoice_Report.xlsx"))
+
+# Read File 2
+df2 = (spark.read
+       .option("header", "true")
+       .option("inferSchema", "true")
+       .excel("/Volumes/workspace/bronze/source_system/ERP_FILES/Dealer_Invoice_Report1.xlsx"))
+
+# Merge (Append)
+df_final = df1.unionByName(df2)
+
+# Display result
+df_final.display()
 
 ### Write to Bronze Layer table 1.bronze.dealer_invoices
-df.write.mode("overwrite").saveAsTable("bronze.dealer_invoices")
+df_final.write.mode("overwrite").saveAsTable("bronze.dealer_invoices")
 
 -------------------------------------------------------------------------------------------------------
 
@@ -40,5 +52,15 @@ df = (spark.read.option("header","true")
 df.display()
 
 df.write.mode("overwrite").saveAsTable("bronze.stock_report")
+
+---------------------------------------------------------------------------------------------------------
+### Reading 4.Wholesale_VOC_Bangalore.xlsx
+df = (spark.read.option("header","true")
+      .option("inferSchema","true")
+      .excel("/Volumes/workspace/bronze/source_system/ERP_FILES/Wholesale_VOC_Bangalore.xlsx"))
+df.display()
+
+### Writing 4.Wholesale_VOC_Bangalore.xlsx
+df.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable("bronze.wholesale_voc_bangalore")
 
 ---------------------------------------------------------------------------------------------------------
